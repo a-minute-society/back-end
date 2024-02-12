3ff26de77db673840a1eef59756298fe0a5a47e3
MinseoKangQ <kms02171@naver.com>
2024-02-13 00:51:22 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/project/aminutesociety/AMinuteSocietyApplication.java
@@ -0,0 +1,13 @@
+package com.project.aminutesociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMinuteSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AMinuteSocietyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/aminutesociety/AMinuteSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aminutesociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AMinuteSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
