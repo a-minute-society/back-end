7c3fd3cef46aff7cc0c259337ebeb1ade2971754
MinseoKangQ <kms02171@naver.com>
2024-02-13 15:13:52 +0900

    :card_file_box: 유저와 카테고리 중간 테이블 작성

--- src/main/java/com/project/aminutesociety/user/entity/User.java
+++ src/main/java/com/project/aminutesociety/user/entity/User.java
@@ -1,9 +1,13 @@
 package com.project.aminutesociety.user.entity;
 
 import com.project.aminutesociety.user.dto.UserSignUpDto;
+import com.project.aminutesociety.usercategory.entity.UserCategory;
 import com.project.aminutesociety.util.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 setter 사용 금지
@@ -23,6 +27,9 @@ public class User extends BaseEntity {
     private String userId;
     private String userName;
     private String userPw;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<UserCategory> userCategories = new ArrayList<>();
 
     public static User signUp(UserSignUpDto.Req req) { return req.toEntity(); }
 }
---
+++ src/main/java/com/project/aminutesociety/usercategory/entity/UserCategory.java
@@ -0,0 +1,29 @@
+package com.project.aminutesociety.usercategory.entity;
+
+import com.project.aminutesociety.category.Category;
+import com.project.aminutesociety.user.entity.User;
+import com.project.aminutesociety.util.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "USERS_CATEGORIES")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserCategory extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "user_category_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
