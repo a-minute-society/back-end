9149cc7413f04cd891ef8f75cfdc9ef1ed7d8dd7
7beunseo <kimes0403@gmail.com>
2024-02-13 17:07:47 +0900

    ✨ 카테고리 데이터베이스 초기화
    카테고리 초기 데이터 설정 - 한글 깨짐 문제 해결 필요

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO CATEGORIES (category_name)
+VALUES ('IT'),
+       ('Economy/Finance'),
+       ('lifestyle'),
+       ('Cooking/Baking'),
+       ('Exercise/Fitness'),
+       ('Photo/Video'),
+       ('programming'),
+       ('marketing'),
+       ('design'),
+       ('Video/Mindset');
+
