b7319aba11c713a63e514d37f1069a4eb210c26a
7beunseo <kimes0403@gmail.com>
2024-02-15 21:52:32 +0900

    ✨ 마이페이지 접속 API 작성

--- src/main/java/com/project/aminutesociety/attendance/repository/AttendanceRepository.java
+++ src/main/java/com/project/aminutesociety/attendance/repository/AttendanceRepository.java
@@ -1,9 +1,13 @@
 package com.project.aminutesociety.attendance.repository;
 
 import com.project.aminutesociety.domain.Attendance;
+import com.project.aminutesociety.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    List<Attendance> findByUser(User user);
 }
--- src/main/java/com/project/aminutesociety/attendance/service/AttendanceService.java
+++ src/main/java/com/project/aminutesociety/attendance/service/AttendanceService.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface AttendanceService {
     ResponseEntity<ApiResponse<?>> setSaveTime(String userId, SetAttendanceDto setAttendanceDto);
+
+    ResponseEntity<ApiResponse<?>> readAttendanceAll(String userId);
 }
